b3774dfdb1ce2082e2e53d8f91d0a1fc15d0fcb0
Aniket Bhattacharyea <aniketmail669@gmail.com>
2020-05-24 22:07:41 +0530

    Improve regex

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ subreddit = reddit.subreddit("pythonforengineers")
 # Regex to match
 # First match inside backticks. If that fails fallback to match until first space.
 # TODO: Does it match every topic?
-match_text_with_backtick = "\`:(h|he|hel|help) (.*?)\`"
+match_text_with_backtick = r"`:(h|he|hel|help) (.*?)`"
 match_text_with_space = ":(h|he|hel|help) ([^\s]+)"
 
 match_re_space = re.compile(match_text_with_space, re.IGNORECASE)
@@ -20,18 +20,36 @@ match_re_backtick = re.compile(match_text_with_backtick, re.IGNORECASE)
 
 # Track new comments
 for comment in subreddit.stream.comments(skip_existing=True):
+    # Don't reply to own comment, although it is highly unlikely to contain the trigger terms.
+    if comment.author.name == 'vim-help-bot':
+        continue
     matches = match_re_backtick.findall(comment.body)
     if len(matches) == 0:
         matches = match_re_space.findall(comment.body)
+    if len(matches) == 0:
+        continue
+    text = ""
+    replied_topics = []
     for match in matches:
         topic = match[1]
+
+        # Already replied. Skip
+        if topic in replied_topics:
+            continue
         # Create the link by by adjoining .txt after the matched part.
         # TODO: Is this the correct approach?
         link = "https://vimhelp.org/{}.txt.html".format(topic)
         request = requests.get(link)
         if request.status_code == 200:
-            reply = "Help for {}: {} \n\n I'm a bot.".format(topic, link)
-            print(reply)
-            comment.reply(reply)
+            reply = "Help for {}: {} \n\n".format(topic, link)
+            text += reply
+            replied_topics.append(topic)
         else:
             print("Link not found")
+    
+    # Link not found for all the topics
+    if len(text) == 0:
+        continue
+
+    text += "I'm a bot."
+    comment.reply(text)
