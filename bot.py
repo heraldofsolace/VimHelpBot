9593ef5874adbef399d8e5f622d81f2bfd93b406
Aniket Bhattacharyea <aniketmail669@gmail.com>
2020-05-25 00:33:22 +0530

    Add tags db

--- bot.py
+++ bot.py
@@ -3,12 +3,15 @@
 import praw
 import re
 import requests
+import sqlite3
 
 reddit = praw.Reddit('bot1')
 
 # Currently point to this subreddit which allows bot testing
 subreddit = reddit.subreddit("pythonforengineers")
 
+conn = sqlite3.connect("tags.db")
+c = conn.cursor()
 # Regex to match
 # First match inside backticks. If that fails fallback to match until first space.
 # TODO: Does it match every topic?
@@ -21,8 +24,8 @@ match_re_backtick = re.compile(match_text_with_backtick, re.IGNORECASE)
 # Track new comments
 for comment in subreddit.stream.comments(skip_existing=True):
     # Don't reply to own comment, although it is highly unlikely to contain the trigger terms.
-    if comment.author.name == 'vim-help-bot':
-        continue
+    #if comment.author.name == 'vim-help-bot':
+     #   continue
     matches = match_re_backtick.findall(comment.body)
     if len(matches) == 0:
         matches = match_re_space.findall(comment.body)
@@ -38,14 +41,24 @@ for comment in subreddit.stream.comments(skip_existing=True):
             continue
         # Create the link by by adjoining .txt after the matched part.
         # TODO: Is this the correct approach?
-        link = "https://vimhelp.org/{}.txt.html".format(topic)
-        request = requests.head(link)
-        if request.ok:
-            reply = "Help for {}: {} \n\n".format(topic, link)
-            text += reply
-            replied_topics.append(topic)
+        t = (topic,)
+        
+        # Search in DB
+        result = c.execute('select * from tags where tag=?', t).fetchone()
+        print(result)
+        if result is None:
+            print("Tag not found")
         else:
-            print("Link not found")
+            doc = result[0]
+            link = "https://vimhelp.org/{}.txt.html#{}".format(doc,topic)
+            
+            request = requests.head(link)
+            if request.ok:
+                reply = "Help for {}: {} \n\n".format(topic, link)
+                text += reply
+                replied_topics.append(topic)
+            else:
+                print("Link not found")
     
     # Link not found for all the topics
     if len(text) == 0:
---
+++ help_tag_extractor.py
@@ -0,0 +1,19 @@
+import re
+import glob
+import sqlite3
+
+conn = sqlite3.connect('tags.db')
+c = conn.cursor()
+files = glob.glob("/home/aniket/vim/runtime/doc/*.txt")
+for doc in files:
+    with open(doc) as f:
+        text = f.read()
+        match_re = re.compile(r'(?<=\s)\*(.*?)\*(?=\s)')
+        matches = match_re.findall(text)
+        for m in matches:
+            doc = doc.split("/")[-1].split(".")[0]
+            t = (doc, m)
+            c.execute("INSERT INTO tags VALUES (?,?)", t)
+            print("{} => {}".format(doc, m))
+conn.commit()
+conn.close()
