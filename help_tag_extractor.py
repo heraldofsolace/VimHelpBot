2d1d649de0b1660f09f989bfe45edcbfb4145efa
Aniket Bhattacharyea <aniketmail669@gmail.com>
2020-05-25 12:46:38 +0530

    Filenames are tags too

--- help_tag_extractor.py
+++ help_tag_extractor.py
@@ -8,7 +8,8 @@ files = glob.glob("/home/aniket/vim/runtime/doc/*.txt")
 for doc in files:
     with open(doc) as f:
         text = f.read()
-        match_re = re.compile(r'(?<=\s)\*(.*?)\*(?=\s)')
+        
+        match_re = re.compile(r'(?:^|\s)\*(.*?)\*(?=\s)')
         matches = match_re.findall(text)
         for m in matches:
             doc = doc.split("/")[-1].split(".")[0]
