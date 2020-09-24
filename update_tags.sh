a71f19b5e94e3398fefc577bb678f5d92982a56c
jakergrossman <jake.r.grossman@gmail.com>
2020-09-24 09:30:28 -0500

    Don't try to pull if repository was cloned

--- update_tags.sh
+++ update_tags.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# clone (Neo)Vim if it doesn't exist
-[ -d third_party/vim ] || git clone https://github.com/vim/vim.git third_party/vim
-[ -d third_party/neovim ] || git clone https://github.com/neovim/neovim.git third_party/neovim
+# clone Vim if it doesn't exist, else pull most recent version
+if [ ! -d third_party/vim ]; then
+    git clone https://github.com/vim/vim.git third_party/vim
+else
+    git -C third_party/vim pull
+fi
 
-# make sure (Neo)Vim are up to date
-git -C third_party/vim pull
-git -C third_party/neovim pull
+# clone Neovim if it doesn't exist, else pull most recent version
+if [ ! -d third_party/neovim ]; then
+    git clone https://github.com/neovim/neovim.git third_party/neovim
+else
+    # store output of git pull
+    nvim_status=$(git -C third_party/neovim pull | tee /dev/tty)
+fi
 
-# build helptags for Neovim (Vim repo comes with 'tags' file)
-vim --clean -e --cmd 'helptags third_party/neovim/runtime/doc | quit'
+# build helptags for Neovim, if needed (Vim repo comes with 'tags' file)
+if [ "$nvim_status" != "Already up to date." ]; then
+    vim --clean -e --cmd 'helptags third_party/neovim/runtime/doc | quit'
+fi
 
 python help_tag_extractor.py
